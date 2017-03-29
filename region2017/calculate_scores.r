41f00649011fb49645b8258ce1466c30e75c3c4c
Remi Daigle <remi.daigle@dal.ca>
2017-03-29 12:49:01 -0300

    eliminate need to setwd()

--- region2017/calculate_scores.r
+++ region2017/calculate_scores.r
@@ -1,25 +1,25 @@
-## calculate_scores.R 
+## calculate_scores.R
 
 ## calculate_scores.R ensures all files are properly configured and calculates OHI scores.
   ## - configure_toolbox.r ensures your files are properly configured. It is a script in your repository.
-  ## - CalculateAll() calculates OHI scores. It is a function in the `ohicore` R package 
-  ##   (this can be written in R as `ohicore::CalculateAll()`).  
+  ## - CalculateAll() calculates OHI scores. It is a function in the `ohicore` R package
+  ##   (this can be written in R as `ohicore::CalculateAll()`).
 
 ## When you begin, configure_toolbox.r and CalculateAll() will calculate scores using
 ## the 'templated' data and goal models provided. We suggest you work
 ## goal-by-goal as you prepare data in the prep folder and develop goal models
-## in functions.r. Running configure_toolbox.r and a specific goal model line-by-line 
+## in functions.r. Running configure_toolbox.r and a specific goal model line-by-line
 ## in functions.R is a good workflow.
 
 ## run the configure_toolbox.r script to check configuration
-wd <- 'region2016'
+wd <- 'region2017'
 source(file.path(wd,'configure_toolbox.r'))
 
 ## calculate scenario scores
 scores = ohicore::CalculateAll(conf, layers)
 
 ## save scores as scores.csv
-write.csv(scores, 'scores.csv', na='', row.names=F)
+write.csv(scores, file.path(wd,'scores.csv'), na='', row.names=F)
 
 
 ## create figures ----
@@ -34,8 +34,8 @@ PlotMapMulti(scores       = scores,
 
 
 ## Make Flower Plots for each region
-PlotFlowerMulti(scores          = scores, 
-                rgns_to_plot    = unique(scores$region_id), 
+PlotFlowerMulti(scores          = scores,
+                rgns_to_plot    = unique(scores$region_id),
                 assessment_name = 'Global')
 
 
--- region2017/configure_toolbox.r
+++ region2017/configure_toolbox.r
@@ -1,10 +1,10 @@
 ## configure_toolbox.r
 
-## configure_toolbox.r ensures all files in your repo are properly configured. 
-## It must be sourced before calculating OHI scores with ohicore::CalculateAll(); 
+## configure_toolbox.r ensures all files in your repo are properly configured.
+## It must be sourced before calculating OHI scores with ohicore::CalculateAll();
 ## it can be sourced here or is also sourced from calculate_scores.r.
 
-## You are encouraged to use this script when developing individual goal models. A good workflow is: 
+## You are encouraged to use this script when developing individual goal models. A good workflow is:
   ## 1. prepare data layers in the /prep folders (script as much as possible in R)
   ## 2. register data layers in layers.csv and save them in /layers folder
   ## 3. source configure_repo.r to ensure proper configuration
@@ -19,8 +19,8 @@ pkgs_installed <- sapply(pkgs_check, FUN = function(x) library(x, character.only
 conf = ohicore::Conf(file.path(wd, 'conf'))
 
 ## check that scenario layers files in the \layers folder match layers.csv registration. Layers files are not modified.
-ohicore::CheckLayers(file.path(wd, 'layers.csv'), 'layers', flds_id=conf$config$layers_id_fields)
+ohicore::CheckLayers(file.path(wd, 'layers.csv'), file.path(wd,'layers'), flds_id=conf$config$layers_id_fields)
 
 ## load scenario layers for ohicore to access. Layers files are not modified.
-layers = ohicore::Layers(file.path(wd, 'layers.csv'), 'layers')
+layers = ohicore::Layers(file.path(wd, 'layers.csv'), file.path(wd,'layers'))
 
